b7a9e74f24de88385658d39f8b4f4e2d313dcbde
mahamudul001 <mahamuduljalal@gmail.com>
2023-09-12 11:51:02 +0600

    task-1 done

---
+++ Task-1/personal_info.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Personal Information</title>
+</head>
+<body>
+
+<?php
+// Define variables
+
+$name = "Mahamudul Hasan";
+$age = 20;
+$country = "Bangladesh";
+$introduction = "Hello, I'm Mahamudul. I'm a web developer from the Bangladesh. Nice to meet you!";
+?>
+
+<h1>Personal Information</h1>
+<p><strong>Name:</strong> <?php echo $name; ?></p>
+<p><strong>Age:</strong> <?php echo $age; ?></p>
+<p><strong>Country:</strong> <?php echo $country; ?></p>
+<p><strong>Introduction:</strong> <?php echo $introduction; ?></p>
+
+</body>
+</html>
